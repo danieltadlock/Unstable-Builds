00d26f10b5727f68bd556854b460fbbb48f4edfd
N3ther <thenetherreactor@gmail.com>
2018-03-07 19:36:04 -0500

    Add files via upload

--- save feature.bat
+++ save feature.bat
@@ -1,7 +1,8 @@
 @echo off
-
 Set savelevel=
-@echo %var%>savefile.txt
-
+@echo %savelevel%>savefile.txt
 For /f "tokens*" %%i in (%USERPROFILE%/Desktop/QuestlightSaves/savefile.txt) do @echo %%i
+set name=
+@echo %name%>savename.txt
+For /f "tokens*" %%i in (%USERPROFILE%/Desktop/QuestlightSaves/savename.txt) do @echo %%i
 Pause
