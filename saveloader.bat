01c7449ca2f945f1cb958aed2f4b6fc634849f0c
N3ther <thenetherreactor@gmail.com>
2018-03-08 12:46:14 -0500

    Add files via upload

--- saveloader.bat
+++ saveloader.bat
@@ -1,12 +1,18 @@
 :saveDetector
-if exist %USERPROFILE%/Desktop/savefile.txt
-	if exist %USERPROFILE%/Desktop/savename.txt
-	set /P c=Save detected. Would you like to load it?[Y/N]
-		if /I "%c%" EQU "Y" goto :saveloader
-		if /T "%c%" EQU "N" goto :choice
-		goto 
-			:saveloader
-				set /p save=<savefile.txt
-				set /p name=<savename.txt
-				goto :%save%
-				
+if exist %USERPROFILE%/Desktop/savefile.txt (
+ goto :saveDetectorName
+) else (
+ goto :name
+	:saveDetectorName
+		if exist %USERPROFILE%/Desktop/savename.txt (
+		set /P c=Save detected. Would you like to load it?[Y/N]
+			if /I "%c%" EQU "Y" goto :saveloader
+			if /T "%c%" EQU "N" goto :choice
+			goto :saveDetectorName
+			) else (
+			goto :noName
+				:saveloader
+					set /p save=<savefile.txt
+					set /p name=<savename.txt
+					goto :%save%
+					
